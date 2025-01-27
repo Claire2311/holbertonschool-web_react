17241299e34cbb933d5c5f89113516968c38f3b6
Claire2311 <claire.girard@ieseg.fr>
2025-01-27 14:23:26 +0100

    Task 8

---
+++ TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,10 @@
+import { RowID } from "./interface";
+import { RowElement } from "./interface";
+
+declare function insertRow(row: RowElement): number;
+
+declare function deleteRow(rowId: RowID): void;
+
+declare function updateRow(rowId: RowID, row: RowElement): RowID;
+
+export { insertRow, deleteRow, updateRow };
