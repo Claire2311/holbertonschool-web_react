5558a9f62371b7c9652635138352d8e2238e3e91
Claire2311 <claire.girard@ieseg.fr>
2025-01-27 12:18:04 +0100

    Task 4

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -42,3 +42,27 @@ const director1: Directors = {
 console.log(director1);
 
 console.log(printTeacher("John", "Doe"));
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
