4fd343b19e9e69aebb910f4f43687ed76cf18116
Claire2311 <claire.girard@ieseg.fr>
2025-01-30 11:00:15 +0100

    Task 2

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -1,14 +1,8 @@
-import path from "path";
-import { fileURLToPath } from "url";
+const path = require("path");
 
-const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
-const __dirname = path.dirname(__filename); // get the name of the directory
-
-export default {
+module.exports = {
   entry: "./js/dashboard_main",
-  //   entry: {
-  //     main: path.resolve(__dirname, "./js/dashboard_main"),
-  //   },
+
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
