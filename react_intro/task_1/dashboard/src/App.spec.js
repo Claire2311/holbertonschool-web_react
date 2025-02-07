d582b6c6b35ff6595f56e24020695f1b2ae17f3d
Claire2311 <claire.girard@ieseg.fr>
2025-02-07 15:38:08 +0100

    Task 2

---
+++ react_intro/task_1/dashboard/src/App.spec.js
@@ -0,0 +1,33 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+describe("App", () => {
+  it("Render h1 with good text", async () => {
+    render(<App />);
+
+    const titleH1 = screen.getByRole("heading", {
+      level: 1,
+      name: /School Dashboard/i,
+    });
+
+    expect(titleH1).toBeInTheDocument();
+  });
+
+  it("paragraphs should have the correct text", async () => {
+    render(<App />);
+
+    const p1 = screen.getByText(/login to access the full dashboard/i);
+    const p2 = screen.getByText(/copyright 2024 - holberton school/i);
+
+    expect(p1).toBeInTheDocument();
+    expect(p2).toBeInTheDocument();
+  });
+
+  it("Should render the logo image", async () => {
+    render(<App />);
+
+    const image = screen.getByAltText(/holberton logo/i);
+
+    expect(image).toBeInTheDocument();
+  });
+});
