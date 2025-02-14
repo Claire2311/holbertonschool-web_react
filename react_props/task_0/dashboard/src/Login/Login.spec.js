c9c6f686624d30403327f4c7b2416d7520c01fe8
Claire2311 <claire.girard@ieseg.fr>
2025-02-14 12:26:48 +0100

    Task 0

---
+++ react_props/task_0/dashboard/src/Login/Login.spec.js
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react";
+import Login from "./Login";
+
+describe("App", () => {
+  it("paragraphs should have the correct text", async () => {
+    render(<Login />);
+
+    const p = screen.getByText(/login to access the full dashboard/i);
+
+    expect(p).toBeInTheDocument();
+  });
+
+  it("should render 2 input elements", async () => {
+    render(<Login />);
+    const inputEmail = screen.getByLabelText(/email/i, { selector: "input" });
+    const inputPassword = screen.getByLabelText(/password/i, {
+      selector: "input",
+    });
+
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
+  });
+
+  it("should render 2 label element with the correct text", async () => {
+    render(<Login />);
+    const labelEmail = screen.getByText(/email:/i);
+    const labelPassword = screen.getByText(/password:/i);
+
+    expect(labelEmail).toBeInTheDocument();
+    expect(labelPassword).toBeInTheDocument();
+  });
+
+  it("should render a button with the correct text", async () => {
+    render(<Login />);
+
+    const button = screen.getByRole("button", { name: /ok/i });
+    expect(button).toBeInTheDocument();
+  });
+});
