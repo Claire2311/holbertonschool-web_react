f0102e76070c0f6ae67ebc4d22af2e3b4706cba2
Claire2311 <claire.girard@ieseg.fr>
2025-03-25 17:30:54 +0100

    Task 1

--- react_state/task_1/dashboard/src/Login/Login.jsx
+++ react_state/task_1/dashboard/src/Login/Login.jsx
@@ -1,24 +1,78 @@
+import React from "react";
 import { StyleSheet, css } from "aphrodite";
 
-function Login() {
-  return (
-    <>
-      <div className={css(styles.loginBody, styles.small)}>
-        <p>Login to access the full dashboard</p>
-        <label htmlFor="email">
-          Email:
-          <input type="mail" id="email" />
-        </label>
-        <label htmlFor="password">
-          Password:
-          <input type="password" id="password" />
-        </label>
-        <button type="button" className={css(styles.button)}>
-          OK
-        </button>
-      </div>
-    </>
-  );
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      email: "",
+      password: "",
+      enableSubmit: false,
+    };
+    this.handleLoginSubmit = this.handleLoginSubmit.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
+
+  emailIsValid = false;
+  passwordIsValid = false;
+
+  handleLoginSubmit(e) {
+    e.preventDefault();
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleEnableSubmit() {
+    if (this.emailIsValid && this.passwordIsValid) {
+      this.setState({ enableSubmit: true });
+    } else {
+      this.setState({ enableSubmit: false });
+    }
+  }
+
+  handleChangeEmail(e) {
+    this.setState({ email: e.target.value });
+    this.emailIsValid = /^[^@]+@[^@]+\.[^@]+$/.test(this.state.email);
+    this.handleEnableSubmit();
+  }
+
+  handleChangePassword(e) {
+    this.setState({ password: e.target.value });
+    this.passwordIsValid = this.state.password.length >= 8;
+    this.handleEnableSubmit();
+  }
+
+  render() {
+    return (
+      <>
+        <div className={css(styles.loginBody, styles.small)}>
+          <p>Login to access the full dashboard</p>
+          <form onSubmit={this.handleLoginSubmit}>
+            <label htmlFor="email">
+              Email:
+              <input type="mail" id="email" onChange={this.handleChangeEmail} />
+            </label>
+            <label htmlFor="password">
+              Password:
+              <input
+                type="password"
+                id="password"
+                onChange={this.handleChangePassword}
+              />
+            </label>
+            <input
+              id="inputSubmit"
+              type="submit"
+              value="OK"
+              className={css(styles.button)}
+              disabled={!this.state.enableSubmit}
+            />
+          </form>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Login;
--- react_state/task_1/dashboard/src/Login/Login.spec.js
+++ react_state/task_1/dashboard/src/Login/Login.spec.js
@@ -29,7 +29,7 @@ describe("App", () => {
   });
 
   it("should render a button with the correct text", async () => {
-    render(<Login />);
+    render(<Login enableSubmit={true} />);
 
     const button = screen.getByRole("button", { name: /ok/i });
     expect(button).toBeInTheDocument();
@@ -42,5 +42,26 @@ describe("App", () => {
 
     fireEvent.click(labelEmail);
     expect(inputEmail).toHaveFocus();
+  });
+
+  it("submit input is disabled by default", () => {
+    render(<Login />);
+    const submitInput = screen.queryByRole("button", { name: /ok/i });
+    expect(submitInput).toHaveAttribute("disabled");
+  });
+
+  it("submit input becomes enabled only after both the Email and Password inputs meet the required criteria", () => {
+    render(<Login />);
+
+    const inputEmail = screen.getByLabelText(/email/i, { selector: "input" });
+    const inputPassword = screen.getByLabelText(/password/i, {
+      selector: "input",
+    });
+
+    fireEvent.change(inputEmail, { target: { value: "c.gi@mail.com" } });
+    fireEvent.change(inputPassword, { target: { value: "password123" } });
+
+    const submitInput = screen.getByRole("button");
+    expect(submitInput).toBeInTheDocument();
   });
 });
