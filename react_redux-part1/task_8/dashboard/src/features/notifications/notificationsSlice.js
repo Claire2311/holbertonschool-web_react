95116486ba54f1fba3f216ac181d32ba75765a3d
Claire2311 <claire.girard@ieseg.fr>
2025-04-04 12:52:14 +0200

    Initiate task 11

---
+++ react_redux-part1/task_8/dashboard/src/features/notifications/notificationsSlice.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getLatestNotification } from "../../utils/utils";
+
+const initialState = {
+  notifications: [],
+  displayDrawer: true,
+};
+
+const API_BASE_URL = "http://localhost:5173";
+
+const ENDPOINTS = {
+  notifications: `${API_BASE_URL}/notifications.json`,
+};
+
+export const fetchNotifications = createAsyncThunk(
+  "notifications/fetchNotifications",
+  async () => {
+    try {
+      const response = await axios.get(ENDPOINTS.notifications);
+      const data = response.data;
+
+      // Update notification with id 3 to include the latest notification
+      const updatedNotifications = data.map((notification) =>
+        notification.id === 3
+          ? { ...notification, ...getLatestNotification() }
+          : notification
+      );
+
+      return updatedNotifications;
+    } catch (error) {
+      console.error("Error fetching notifications:", error);
+      throw error;
+    }
+  }
+);
+
+const notificationsSlice = createSlice({
+  name: "notifications",
+  initialState,
+  reducers: {
+    markNotificationAsRead: (state, action) => {
+      const notificationId = action.payload;
+      state.notifications = state.notifications.filter(
+        (notification) => notification.id !== notificationId
+      );
+      console.log(`Notification ${action.payload} has been marked as read`);
+    },
+    showDrawer: (state) => {
+      state.displayDrawer = true;
+    },
+    hideDrawer: (state) => {
+      state.displayDrawer = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchNotifications.fulfilled, (state, action) => {
+      state.notifications = action.payload;
+    });
+  },
+});
+
+export const { markNotificationAsRead, showDrawer, hideDrawer } =
+  notificationsSlice.actions;
+export default notificationsSlice.reducer;
