a903d615a25a89d99081eb7f2746231d228718f8
Claire2311 <claire.girard@ieseg.fr>
2025-04-11 16:06:19 +0200

    Task 0

--- react_hooks/task_0/dashboard/src/Header/Header.jsx
+++ react_hooks/task_0/dashboard/src/Header/Header.jsx
@@ -1,5 +1,5 @@
 import holbertonLogo from "../assets/holberton-logo.jpg";
-// import { StyleSheet, css } from "aphrodite";
+import { StyleSheet, css } from "aphrodite";
 import newContext from "../Context/context";
 import { useContext } from "react";
 
@@ -7,9 +7,13 @@ const Header = () => {
   const { userObject, logOut } = useContext(newContext);
 
   return (
-    <div>
-      <div>
-        <img src={holbertonLogo} alt="holberton logo" />
+    <>
+      <div className={css(styles.header)}>
+        <img
+          src={holbertonLogo}
+          alt="holberton logo"
+          className={css(styles.img)}
+        />
         <h1>School dashboard</h1>
       </div>
       {userObject.isLoggedIn && (
@@ -20,47 +24,21 @@ const Header = () => {
           </a>
         </p>
       )}
-    </div>
+    </>
   );
 };
 
 export default Header;
 
-// const styles = StyleSheet.create({
-//   header: {
-//     display: "flex",
-//     alignItems: "center",
-//     color: "#e1003c",
-//     borderBottom: "2px solid #e1003c",
-//     fontFamily: "Arial, Helvetica, sans-serif",
-//   },
-//   img: {
-//     height: "200px",
-//   },
-// });
-
-// import { useContext } from "react";
-// import "./Header.css";
-// import HolbertonLogo from "../assets/holberton-logo.jpg";
-// import { newContext } from "../Context/context";
-
-// const Header = () => {
-//   const { user, logOut } = useContext(newContext);
-//   return (
-//     <div className="App-header">
-//       <img className="App-logo" src={HolbertonLogo} alt="holberton logo" />
-//       <h1>School dashboard</h1>
-//       {user.isLoggedIn && (
-//         <section id="logoutSection">
-//           Welcome {user.email} (
-//           <a href="" onClick={logOut}>
-//             logout
-//           </a>
-//           )
-//         </section>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Header;
+const styles = StyleSheet.create({
+  header: {
+    display: "flex",
+    alignItems: "center",
+    color: "#e1003c",
+    borderBottom: "2px solid #e1003c",
+    fontFamily: "Arial, Helvetica, sans-serif",
+  },
+  img: {
+    height: "200px",
+  },
+});
