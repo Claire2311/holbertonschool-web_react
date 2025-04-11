a0d68668feb14dc907569c8f949b23d87159146b
Claire2311 <claire.girard@ieseg.fr>
2025-04-11 16:24:02 +0200

    task 0 fix Login

--- react_hooks/task_0/dashboard/src/Login/Login.jsx
+++ react_hooks/task_0/dashboard/src/Login/Login.jsx
@@ -32,14 +32,16 @@ class Login extends React.Component {
   }
 
   handleChangeEmail(e) {
-    this.setState({ email: e.target.value });
-    this.emailIsValid = /^[^@]+@[^@]+\.[^@]+$/.test(this.state.email);
+    const newEmail = e.target.value;
+    this.emailIsValid = /^[^@]+@[^@]+\.[^@]+$/.test(newEmail);
+    this.setState({ email: newEmail });
     this.handleEnableSubmit();
   }
 
   handleChangePassword(e) {
-    this.setState({ password: e.target.value });
-    this.passwordIsValid = this.state.password.length >= 8;
+    const newPassword = e.target.value;
+    this.passwordIsValid = newPassword.length >= 8;
+    this.setState({ password: newPassword });
     this.handleEnableSubmit();
   }
 
